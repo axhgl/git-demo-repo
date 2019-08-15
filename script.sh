0a25e4b5b237aeddd205a872964a4f777e1b3c32
Argjend Xhigoli <argjendxhigoli@gmail.com>
2019-08-15 15:14:56 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,5 +9,23 @@ ls -alh
 echo "Download files" 
 rm -rf *
 echo "restart apache2"
-service apache2 restart
+#service apache2 restart
 echo "finito"
+echo "Download wordpress"
+git clone https://github.com/WordPress/WordPress.git
+echo "list files"
+ls -alh
+echo "Download files" 
+rm -rf *
+echo "Download wordpress"
+git clone https://github.com/WordPress/WordPress.git
+echo "list files"
+ls -alh
+echo "Download files" 
+rm -rf *
+echo "Download wordpress"
+git clone https://github.com/WordPress/WordPress.git
+echo "list files"
+ls -alh
+echo "Download files" 
+rm -rf *
